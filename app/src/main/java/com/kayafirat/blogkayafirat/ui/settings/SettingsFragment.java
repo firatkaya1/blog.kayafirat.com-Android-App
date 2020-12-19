5162a1c467db0a90966590f4663ca5af5d6c41a2
firatkaya1 <firat.kaya@mail.com>
2020-12-19 07:27:42 +0300

    user settings added

--- app/src/main/java/com/kayafirat/blogkayafirat/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/kayafirat/blogkayafirat/ui/settings/SettingsFragment.java
@@ -4,33 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.kayafirat.blogkayafirat.R;
+import com.kayafirat.blogkayafirat.model.User;
+import com.kayafirat.blogkayafirat.service.IUserService;
+import com.kayafirat.blogkayafirat.service.impl.UserService;
 import com.kayafirat.blogkayafirat.ui.search.SearchViewModel;
 
 public class SettingsFragment extends Fragment  {
 
     private SearchViewModel searchViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        searchViewModel =
-                new ViewModelProvider(this).get(SearchViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_search);
-        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        searchViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        TextView userId = root.findViewById(R.id.userId);
+        EditText emailAddress = root.findViewById(R.id.emailAddress);
+        EditText userName = root.findViewById(R.id.username);
+        EditText userPassword = root.findViewById(R.id.userPassword);
+
+        IUserService userService = new UserService();
+        User user = userService.getUser(1);
+
+        userId.setText(user.getUserId());
+        emailAddress.setText(user.getUserEmail());
+        userName.setText(user.getUserName());
+        userPassword.setText(user.getUserPassword());
+
         return root;
     }
 }
